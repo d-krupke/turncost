c22576dec8ff197cfb463d40f89cf3a7728cbf87
d-krupke <d.krupke@tu-bs.de>
2018-01-12 13:24:14 +0100

    aux-files

---
+++ code/tests/gtest_main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Dominik Krupke, http://krupke.cc on 6/25/17.
+//
+
+#include <gtest/gtest.h>
+
+int
+main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
